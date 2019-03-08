c2ba2b780f3d07556d32dbd5a6f5f98319b2e18b
Felix Wiegand <fxwiegand@gmail.com>
2019-03-08 14:39:20 +0100

    added test bam and some bam reading code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rust_htslib;
+
 
 #[cfg(test)] mod tests;
 
-
+use rust_htslib::bam;
+use rust_htslib::prelude::*;
 
 #[get("/lisbert/<verb>")]
 fn hello(verb: String) -> String {format!("Lisa {}!", verb)}
 
 fn main() {
     rocket::ignite().mount("/", routes![hello]).launch();
+
+    let mut bam = bam::Reader::from_path(&"data/test.bam").unwrap();
+    let header = bam::Header::from_template(bam.header());
+    let mut out = bam::Writer::from_path(&"data/out.bam", &header).unwrap();
+
+    for r in bam.records() {
+        let record = r.unwrap();
+        if record.is_reverse() {
+            out.write(&record).unwrap();
+        }
+    }
 }
+
+
