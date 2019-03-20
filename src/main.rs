e8f69aefbf92e6314e2823adbb1e9cbbb3b560e7
Felix Wiegand <fxwiegand@gmail.com>
2019-03-20 18:16:53 +0100

    changed flags into BTreeMap

--- src/main.rs
+++ src/main.rs
@@ -24,60 +24,60 @@ fn genome(index: i32) -> Json<Alignment> {Json(read_bam(index))}
 #[get("/api/v1/count")]
 fn count() -> Json<u32> {Json(count_alignments())}
 
-fn decode_flags(code :u16) -> String {
-    let mut flag_string = String::from("");
+fn decode_flags(code :u16) -> BTreeMap<u32, &'static str> {
+    let mut string_map = BTreeMap::new();
 
-    const FLAG_1: &'static str = "1: template having multiple segments in sequencing ";
-    const FLAG_2: &'static str = "2: each segment properly aligned according to the aligner ";
-    const FLAG_3: &'static str = "4: segment unmapped ";
-    const FLAG_4: &'static str = "8: next segment in the template unmapped ";
-    const FLAG_5: &'static str = "16: SEQ being reverse complemented ";
-    const FLAG_6: &'static str = "32: SEQ of the next segment in the template being reverse complemented ";
-    const FLAG_7: &'static str = "64: the first segment in the template ";
-    const FLAG_8: &'static str = "128: the last segment in the template ";
-    const FLAG_9: &'static str = "256: secondary alignment ";
-    const FLAG_10: &'static str = "512: not passing filters, such as platform/vendor quality controls ";
-    const FLAG_11: &'static str = "1024:PCR or optical duplicate ";
-    const FLAG_12: &'static str = "2048: supplementary alignment ";
+    const FLAG_1: &'static str = "template having multiple segments in sequencing";
+    const FLAG_2: &'static str = "each segment properly aligned according to the aligner";
+    const FLAG_3: &'static str = "segment unmapped";
+    const FLAG_4: &'static str = "next segment in the template unmapped";
+    const FLAG_5: &'static str = "SEQ being reverse complemented";
+    const FLAG_6: &'static str = "SEQ of the next segment in the template being reverse complemented";
+    const FLAG_7: &'static str = "the first segment in the template ";
+    const FLAG_8: &'static str = "the last segment in the template";
+    const FLAG_9: &'static str = "secondary alignment";
+    const FLAG_10: &'static str = "not passing filters, such as platform/vendor quality controls";
+    const FLAG_11: &'static str = "PCR or optical duplicate";
+    const FLAG_12: &'static str = "supplementary alignment";
 
     if (0x1 & code) == 0x1 {
-        flag_string.push_str(&FLAG_1);
+        string_map.insert(1 as u32, FLAG_1);
     }
     if  (0x2 & code) == 0x2  {
-        flag_string.push_str(&FLAG_2);
+        string_map.insert(2 as u32, FLAG_2);
     }
     if (0x4 & code) == 0x4 {
-        flag_string.push_str(&FLAG_3);
+        string_map.insert(4 as u32, FLAG_3);
     }
     if  (0x8 & code) == 0x8  {
-        flag_string.push_str(&FLAG_4);
+        string_map.insert(8 as u32, FLAG_4);
     }
     if (0x10 & code) == 0x10 {
-        flag_string.push_str(&FLAG_5);
+        string_map.insert(16 as u32, FLAG_5);
     }
     if  (0x20 & code) == 0x20  {
-        flag_string.push_str(&FLAG_6);
+        string_map.insert(32 as u32, FLAG_6);
     }
     if (0x40 & code) == 0x40 {
-        flag_string.push_str(&FLAG_7);
+        string_map.insert(64 as u32, FLAG_7);
     }
     if  (0x80 & code) == 0x80  {
-        flag_string.push_str(&FLAG_8);
+        string_map.insert(128 as u32, FLAG_8);
     }
     if  (0x100 & code) == 0x100  {
-        flag_string.push_str(&FLAG_9);
+        string_map.insert(256 as u32, FLAG_9);
     }
     if (0x200 & code) == 0x200 {
-        flag_string.push_str(&FLAG_10);
+        string_map.insert(512 as u32, FLAG_10);
     }
     if  (0x400 & code) == 0x400  {
-        flag_string.push_str(&FLAG_11);
+        string_map.insert(1024 as u32, FLAG_11);
     }
     if (0x800 & code) == 0x800 {
-        flag_string.push_str(&FLAG_12);
+        string_map.insert(2048 as u32, FLAG_12);
     }
 
-    flag_string
+    string_map
 }
 
 fn count_alignments()-> u32 {
@@ -100,7 +100,7 @@ fn read_bam(index: i32) -> Alignment {
         sequenz: String::from(""),
         position: 0,
         cigar_str: String::from(""),
-        flags: String::from(""),
+        flags: BTreeMap::new(),
         name: String::from(""),
     };
 
@@ -164,13 +164,17 @@ struct Alignment {
     sequenz: String,
     position: i32,
     cigar_str: String,
-    flags: String,
+    flags: BTreeMap<u32, &'static str>,
     name: String,
 }
 
 impl fmt::Display for Alignment {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {}, {}, {}, {})", self.header, self.sequenz, self.position, self.cigar_str, self.flags,
+        let mut flag_string = String::from("");
+        for (_key, flag) in &self.flags {
+            flag_string.push_str(flag);
+        }
+        write!(f, "({}, {}, {}, {}, {}, {})", self.header, self.sequenz, self.position, self.cigar_str, flag_string,
         self.name)
     }
 }
