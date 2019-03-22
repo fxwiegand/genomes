309839e2ae09f93016df071e52a2d69b4dd86af8
Felix Wiegand <fxwiegand@gmail.com>
2019-03-22 14:58:02 +0100

    changed flag from u32 to u16 and solved decode_flags with an iterative solution

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 #[macro_use] extern crate rocket_contrib;
 //#[macro_use] extern crate rustc_serialize;
 #[macro_use] extern crate serde;
+#[macro_use] extern crate phf_macros;
 
 #[cfg(test)] mod tests;
 
@@ -17,14 +18,16 @@ use std::fmt;
 use std::collections::BTreeMap;
 use rocket_contrib::json::Json;
 use serde::ser::{Serialize, SerializeStruct, Serializer};
+use rocket_contrib::serve::StaticFiles;
+use phf_macros::phf_map;
 
-#[get("/api/v1/alignment/<index>")]
+#[get("/alignment/<index>")]
 fn genome(index: i32) -> Json<Alignment> {Json(read_bam(index))}
 
-#[get("/api/v1/count")]
+#[get("/count")]
 fn count() -> Json<u32> {Json(count_alignments())}
 
-fn decode_flags(code :u16) -> BTreeMap<u32, &'static str> {
+fn decode_flags(code :u16) -> BTreeMap<u16, &'static str> {
     let mut string_map = BTreeMap::new();
 
     const FLAG_1: &'static str = "template having multiple segments in sequencing";
@@ -40,41 +43,24 @@ fn decode_flags(code :u16) -> BTreeMap<u32, &'static str> {
     const FLAG_11: &'static str = "PCR or optical duplicate";
     const FLAG_12: &'static str = "supplementary alignment";
 
-    if (0x1 & code) == 0x1 {
-        string_map.insert(1 as u32, FLAG_1);
-    }
-    if  (0x2 & code) == 0x2  {
-        string_map.insert(2 as u32, FLAG_2);
-    }
-    if (0x4 & code) == 0x4 {
-        string_map.insert(4 as u32, FLAG_3);
-    }
-    if  (0x8 & code) == 0x8  {
-        string_map.insert(8 as u32, FLAG_4);
-    }
-    if (0x10 & code) == 0x10 {
-        string_map.insert(16 as u32, FLAG_5);
-    }
-    if  (0x20 & code) == 0x20  {
-        string_map.insert(32 as u32, FLAG_6);
-    }
-    if (0x40 & code) == 0x40 {
-        string_map.insert(64 as u32, FLAG_7);
-    }
-    if  (0x80 & code) == 0x80  {
-        string_map.insert(128 as u32, FLAG_8);
-    }
-    if  (0x100 & code) == 0x100  {
-        string_map.insert(256 as u32, FLAG_9);
-    }
-    if (0x200 & code) == 0x200 {
-        string_map.insert(512 as u32, FLAG_10);
-    }
-    if  (0x400 & code) == 0x400  {
-        string_map.insert(1024 as u32, FLAG_11);
-    }
-    if (0x800 & code) == 0x800 {
-        string_map.insert(2048 as u32, FLAG_12);
+    let mut flags_map = BTreeMap::new();
+    flags_map.insert(0x1, FLAG_1);
+    flags_map.insert(0x2, FLAG_2);
+    flags_map.insert(0x4, FLAG_3);
+    flags_map.insert(0x8, FLAG_4);
+    flags_map.insert(0x10, FLAG_5);
+    flags_map.insert(0x20, FLAG_6);
+    flags_map.insert(0x40, FLAG_7);
+    flags_map.insert(0x80, FLAG_8);
+    flags_map.insert(0x100, FLAG_9);
+    flags_map.insert(0x200, FLAG_10);
+    flags_map.insert(0x400, FLAG_11);
+    flags_map.insert(0x800, FLAG_12);
+
+    for (flag, text) in flags_map {
+        if (flag & code) == flag {
+            string_map.insert(flag, text);
+        }
     }
 
     string_map
@@ -155,7 +141,12 @@ fn read_bam(index: i32) -> Alignment {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![genome, count]).launch();
+    //rocket::ignite().mount("/", routes![genome, count]).launch();
+    rocket::ignite()
+        .mount("/static", StaticFiles::from("./static"))
+        .mount("/api/v1", StaticFiles::from("./templates/index.html"))
+        .mount("/api/v1", routes![genome, count])
+        .launch();
 }
 
 #[derive(Serialize)]
@@ -164,7 +155,7 @@ struct Alignment {
     sequenz: String,
     position: i32,
     cigar_str: String,
-    flags: BTreeMap<u32, &'static str>,
+    flags: BTreeMap<u16, &'static str>,
     name: String,
 }
 
