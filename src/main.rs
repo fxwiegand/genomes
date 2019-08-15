87afa23673f3b2716681a892363dc1caf1bf3ffb
Felix Wiegand <fxwiegand@gmail.com>
2019-08-15 10:40:26 +0200

    jsonm compression off for benchmarks fix

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,7 @@ fn main() {
         rocket::ignite()
             .manage(args)
             .mount("/",  StaticFiles::from("client"))
-            .mount("/api/v1", routes![reference, alignment, variant, uncompressed_reference])
+            .mount("/api/v1", routes![reference, alignment, variant, uncompressed_reference, uncompressed_alignment, uncompressed_variant])
             .attach(Compression::fairing())
             .launch();
     }
