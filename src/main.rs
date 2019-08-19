3f6ed2dfdacdebb5dbd2a0478acf945a806a230f
Felix Wiegand <fxwiegand@gmail.com>
2019-08-19 17:32:41 +0200

    gzip compression on again (benchmarks done)

--- src/main.rs
+++ src/main.rs
@@ -109,6 +109,7 @@ fn main() {
             .manage(args)
             .mount("/",  StaticFiles::from("client"))
             .mount("/api/v1", routes![reference, alignment, variant, uncompressed_reference, uncompressed_alignment, uncompressed_var])
+            .attach(Compression::fairing())
             .launch();
     }
 }
